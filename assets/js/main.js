d5829c7d90d9fd0ecd4d8b6719bcc107ae3b45f3
ipranshulsingla <52979328+ipranshulsingla@users.noreply.github.com>
2020-03-05 18:02:53 +0530

    Add files via upload

---
+++ assets/js/main.js
@@ -0,0 +1,12 @@
+window.addEventListener('load',bindEvents);
+
+function bindEvents(){
+    addWidth();
+}
+
+function addWidth(){
+    aWidth=screen.availWidth;
+    gmap=document.querySelector("#outshine-gmap");
+    gmap=gmap.children;
+    gmap[0].width=aWidth;
+}
